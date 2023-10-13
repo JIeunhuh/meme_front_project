1fbc1db7b9e69d088d0e9f21fade27525c1b5bcd
JIeunhuh <entks301@naver.com>
2023-10-13 15:37:41 +0900

    design : 헤더, 푸터용 레이아웃 디자인

---
+++ fe-feature-heo/src/styled/Layout.js
@@ -0,0 +1,37 @@
+import styled from 'styled-components'
+/**
+* @param font : font 이름 
+**/
+export const HeaderText = styled.nav`
+color: rgba(102, 102, 102, 0.80);
+font-family: ${props => props.font};
+font-size: 28px;
+font-style: bold;
+font-weight: 700;
+justify-content: center;
+algin-items:center;
+margin-left : 185px;
+
+a {
+    margin-right:53px;
+    }
+`
+// icon 설정
+// export const HeaderIcon = styled.image
+
+export const HeaderButton = styled.button`
+justify-content: center;
+align-items: center;
+border-radius: 8px;
+background-color:${props => props.background || '#FAFAFA'};
+border: 1px solid  ${props => props.color || '#3D1860'};
+color:${props => props.fontcolor || '#3D1860'};
+font-family: ${props => props.font};
+width:6em;
+height:2.5em;
+margin-left:42px;
+`
+export const BtnBox = styled.div`
+  display : flex;
+  margin-right:${props => props.marginright || 'auto'};
+`
