bcf88b983b134fa63a2e3aa9097b8d054d68cf7b
JIeunhuh <entks301@naver.com>
2023-10-13 15:44:06 +0900

    feat : nav bar 추가 중(로그인, 회원가입 버튼 처리 진행 중)

--- fe-feature-heo/src/routes/Layout/Navbar.js
+++ fe-feature-heo/src/routes/Layout/Navbar.js
@@ -1,4 +1,24 @@
+import { NavBox, HeaderText, HeaderButton, BtnBox } from "../../styled"
+import { Link } from 'react-router-dom'
 /** Navbar 컴포넌트 */
 export const Navbar = () => {
-  return <nav></nav>
+
+  return (
+    <NavBox justify='space-between'>
+      <HeaderText font='RedRose'>
+        <Link to={'/'}>Homepage</Link>
+        <Link to={'/search'}>Meme Search</Link>
+        <Link to={'/community'}>Coummunity</Link>
+        <Link to={'/subscribe'}>Subscribe</Link>
+      </HeaderText>
+      <BtnBox marginright='45px'>
+        <HeaderButton font='RedRose' >
+          Log in
+        </HeaderButton>
+        <HeaderButton font="RedRose" background="#3D1860" color="#3D1860" fontcolor="#FFF">
+          Sign Up
+        </HeaderButton>
+      </BtnBox>
+    </NavBox>
+  )
 }
