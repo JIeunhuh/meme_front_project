072d0337b5913060d7924b02058cd49b4209d072
JIeunhuh <entks301@naver.com>
2023-10-13 16:26:41 +0900

    style : 텍스트 수정

--- fe-feature-heo/src/pages/MainPage/TitleContent.js
+++ fe-feature-heo/src/pages/MainPage/TitleContent.js
@@ -1,18 +1,20 @@
-import { Title, SubTitle, BoxContent } from '../../styled'
+import { Title, SubTitle, BoxContent, BtnBox } from '../../styled'
+import { SearchKeyword } from './SearchKeyword';
 
 const TitleContent = () => {
 
     return (
         <main style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <BoxContent height='300px'>
-                <Title font="RedRose" textsize='60px' bold="400">
-                    Elevate Your Sense of Humor
-                </Title>
-                <SubTitle font="Orbit" textsize='25px' bold='400' color='#676767'>
-                    검색 창에 키워드를 입력하세요 ! 상황에 맞는 밈을 추천합니다
-                </SubTitle>
+                    <Title font="RedRose" textsize='60px' bold="400">
+                        Elevate Your Sense of Humor
+                    </Title>
+                    <SubTitle font="Orbit" textsize='25px' bold='400' color='#676767'>
+                        검색 창에 키워드를 입력하세요! 상황에 맞는 밈을 추천합니다
+                    </SubTitle>
+                <SearchKeyword />
             </BoxContent>
-        </main>
+        </main >
     )
 }
 export default TitleContent;
