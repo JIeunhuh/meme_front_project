12cd50097d339f9fd999d14021b1ef6862001455
JIeunhuh <entks301@naver.com>
2023-10-13 15:41:58 +0900

    feat : Mainpage - titleContent의 검색기능 추가 중

---
+++ fe-feature-heo/src/pages/MainPage/TitleContent.js
@@ -0,0 +1,18 @@
+import { Title, SubTitle, BoxContent } from '../../styled'
+
+const TitleContent = () => {
+
+    return (
+        <main style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
+            <BoxContent height='300px'>
+                <Title font="RedRose" textsize='60px' bold="400">
+                    Elevate Your Sense of Humor
+                </Title>
+                <SubTitle font="Orbit" textsize='25px' bold='400' color='#676767'>
+                    검색 창에 키워드를 입력하세요 ! 상황에 맞는 밈을 추천합니다
+                </SubTitle>
+            </BoxContent>
+        </main>
+    )
+}
+export default TitleContent;
