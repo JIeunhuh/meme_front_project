0896efcf26f93d112a8f2b6231b6cbd8756be383
Jieun Huh <entks301@naver.com>
2023-10-27 16:01:13 +0900

    Feat : 메인 페이지 사용자 검색 키워드 순위 표시 기능 작업 중

---
+++ fe-feature-heo/src/pages/MainPage/mainFunction/KeywordTrend.js
@@ -0,0 +1,10 @@
+import { Title } from "../../../styled";
+export const KeyWordTrend = () => {
+    return (
+        <main>
+            <Title marginTop='50px'>
+                Today's Keyword
+            </Title>
+        </main>
+    );
+}
